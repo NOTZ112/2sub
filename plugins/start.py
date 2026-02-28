0f43e56d4aea786e3e564c0c20802bb16df39c77
NOTZ112 <liza3x3i@gmail.com>
2026-03-01 03:07:55 +0530

    Update message text to include new chat handle

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>JOIN @Kambichatiz")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>JOIN @കമ്പിച്ചറിസ് {file_auto_delete}")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
