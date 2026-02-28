820a3299905deafc0e3d3ebe9435d216839124be
NOTZ112 <liza3x3i@gmail.com>
2026-02-28 23:00:19 +0530

    Update message text for file deletion warning

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Video / File To Somewhere Else And Start Downloading There.")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>JOIN</u> ❗️</b>\n\നമ്മുടെ തുണ്ട് / ഗ്രൂപ്പിൽ {file_auto_delete} (മച്ചാൻസ് ).\n\n📌 https://t.me/Kambichatiz.")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
