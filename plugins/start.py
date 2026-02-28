4865d46b371e4991835ffc55f50e4f7cefae766c
NOTZ112 <liza3x3i@gmail.com>
2026-02-28 23:05:36 +0530

    Simplify message sent to user
    Update message format to only include the group link.

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>JOIN</u> ❗️</b>\n\നമ്മുടെ തുണ്ട് / ഗ്രൂപ്പിൽ {file_auto_delete} (മച്ചാൻസ് ).\n\n📌 https://t.me/Kambichatiz.")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>https://t.me/Kambichatiz.")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
